eb58d04a9368c662fb1859fd72c0f38bd4b41554
unknown <onkar.bhasme@gmail.com>
2023-11-30 12:05:16 +0530

    PrimitiveType

--- src/main/java/com/springcore/App.java
+++ src/main/java/com/springcore/App.java
@@ -13,7 +13,12 @@ public class App
     {
         System.out.println( "Hello World!" );
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-        Student s = (Student) context.getBean("student1");
-        System.out.println(s);
+        Student s1 = (Student) context.getBean("student1");
+        Student s2 = (Student) context.getBean("student2");
+        Student s3 = (Student) context.getBean("student3");
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
     }
 }
